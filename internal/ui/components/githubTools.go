3854d0a542c63fb006d63298e7f49114f0f17ada
jgr142 <joshuagisiger142@gmail.com>
2025-07-26 17:21:01 -0400

    feat: add notification changes

--- internal/ui/components/githubTools.go
+++ internal/ui/components/githubTools.go
@@ -1,10 +1,6 @@
 package components
 
 import (
-	"log/slog"
-	"os"
-	"os/exec"
-
 	"github.com/gdamore/tcell/v2"
 	"github.com/jgr142/zeno/internal/git"
 	"github.com/rivo/tview"
@@ -32,22 +28,24 @@ type GithubTools struct {
 	*tview.Flex
 	pages       *tview.Pages
 	projectPath string
-	notify      func(successMsg, errMsg string, isErr bool)
+	notify      func(msg string, isErr bool)
 }
 
 func NewGithubTools(pages *tview.Pages, projectPath string) *GithubTools {
 	// Set Up Flex Structure
 	mainDisplay := tview.NewTextView()
 	notif := NewNotification()
-	skltn := tview.NewFlex().
+	skltn := tview.
+		NewFlex().
 		AddItem(notif, 1, 0, false).
 		AddItem(mainDisplay, 0, 1, true)
 
+		// Configure Pages that you can switch to
 	gt := &GithubTools{
 		skltn,
 		pages,
 		projectPath,
-		notif.DisplaySuccessorErr,
+		notif.DisplayNotification,
 	}
 
 	// Configure mainDisplay
@@ -65,70 +63,77 @@ func (gt *GithubTools) actionListeners(event *tcell.EventKey) *tcell.EventKey {
 	switch event.Rune() {
 	// TODO: Implement handlers
 	case 'r':
-		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
-		slog.SetDefault(logger)
-
-		cmd := exec.Command(
-			"git",
-			"fetch",
-			"origin",
-			"&&",
-			"git",
-			"rebase",
-			"origin/main",
-		)
-		out, err := cmd.Output()
-		if err != nil {
-			slog.Error("Error Running Rebase", "Error", err.Error())
-			gt.SetBackgroundColor(tcell.ColorRed)
-			return nil
-		}
-		gt.SetBackgroundColor(tcell.ColorGreen)
-		slog.Info("Out Data", "StdOut", out)
 	case 'p':
-		git.AddCommitPush("feat: new feature")
+		err := git.AddCommitPush("feat: new feature")
+		if err != nil {
+			gt.notify("ACP Process Failed", true)
+		} else {
+			gt.notify("ACP Success", false)
+		}
+		return nil
 	case 'c':
 		// Make a text input modal show up
 	case 'b':
-		// Again make a text intput modal show up
 	case 's':
 		// status, err := git.Status()
 		return nil
 	case 'l':
 		// log, err := git.Log()
 		// if err != nil {
-		// 	// show err message
-		// 	gt.pages.AddAndSwitchToPage("error modal", NewErrorModal(err.Error()), true)
-		// } else {
-		// 	//show git status page
-		// 	gt.pages.AddAndSwitchToPage("non-err page", NewThemedTextModal(string(status)), true)
+		// 	gt.notify(
+		// 		"Error fetching logs",
+		// 		true,
+		// 	)
+		// 	return nil
 		// }
-		return nil
-		// Show logs
+		//
+		//
+		// return nil
 	case 'u':
 		// assuming we set origin everywhere we go...
 		err := git.Pull()
-		gt.notify(
-			"Successfully pulled from "+gt.projectPath,
-			"Could not pull from "+gt.projectPath,
-			err != nil,
-		)
+		if err != nil {
+			gt.notify(
+				"Could not pull from "+gt.projectPath,
+				true,
+			)
+		} else {
+			gt.notify(
+				"Successfully pulled from "+gt.projectPath,
+				false,
+			)
+		}
+		return nil
 	case 'x':
 		err := git.Stash()
-		gt.notify(
-			"Successfully stashed changes",
-			"Could not stash changes",
-			err != nil,
-		)
+		if err != nil {
+			gt.notify(
+				"Could not stash changes",
+				true,
+			)
+		} else {
+			gt.notify(
+				"Successfully stashed changes",
+				false,
+			)
+		}
+		return nil
 	case 'z':
 		err := git.Pop()
-		gt.notify(
-			"Successfully popped stashed changes",
-			"Could not pop the stashed changes",
-			err != nil,
-		)
+		if err != nil {
+			gt.notify(
+				"Could not pop the stashed changes",
+				true,
+			)
+		} else {
+			gt.notify(
+				"Could not pop the stashed changes",
+				false,
+			)
+		}
+		return nil
 	case 'q':
-		gt.pages.SwitchToPage("project search")
+		gt.pages.SwitchToPage("projects search")
 	}
 	return event
 }
